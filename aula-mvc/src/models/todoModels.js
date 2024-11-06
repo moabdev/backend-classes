18c771656d497852dddd429d63a78e5e6090eb7e
moabdev <macenamoab3@gmail.com>
2024-11-06 15:44:02 -0300

    chore: Add dotenv file

--- aula-mvc/src/models/todoModels.js
+++ aula-mvc/src/models/todoModels.js
@@ -1,10 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const todoSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    completed: { type: Boolean, default: false }
+  titulo: { type: String, required: true },
+  description: { type: String, required: true },
+  done: { type: Boolean, default: false },
 });
 
-const Todo = mongoose.model('Todo', todoSchema);
+const Todo = mongoose.model("Todo", todoSchema);
 
 module.exports = Todo;
